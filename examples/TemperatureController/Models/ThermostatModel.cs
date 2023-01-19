eebdf45ba8b9df540e464797ada1c8cc313343c4
James Coliz <jcoliz@outlook.com>
2023-01-19 11:22:10 -0800

    feat(examples): Take parameter in getMaxMinReport

--- examples/TemperatureController/Models/ThermostatModel.cs
+++ examples/TemperatureController/Models/ThermostatModel.cs
@@ -1,5 +1,7 @@
 using AzDevice.Models;
 using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 public class ThermostatModel : IComponentModel
@@ -27,7 +29,12 @@ public class ThermostatModel : IComponentModel
         if (name != "getMaxMinReport")
             throw new NotImplementedException();
 
-        var result = new MinMaxReportModel() { MaxTemp = 100.0, MinTemp = 200.0, StartTime = DateTimeOffset.Now, EndTime = DateTimeOffset.Now };
+        var json = Encoding.UTF8.GetString(data);
+        DateTimeOffset since = DateTimeOffset.Now - TimeSpan.FromHours(1);
+        if (json.Length > 0)
+            since = JsonSerializer.Deserialize<DateTimeOffset>(json);
+
+        var result = new MinMaxReportModel() { MaxTemp = 100.0, MinTemp = 200.0, StartTime = since, EndTime = DateTimeOffset.Now };
         return Task.FromResult<object>(result);
     }
 
