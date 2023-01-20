d10625d79cbe6ba575aebc67f32ec42037675242
James Coliz <jcoliz@outlook.com>
2023-01-20 09:01:38 -0800

    build: dotnet new worker --name ClimateMonitor

---
+++ examples/ClimateMonitor/Program.cs
@@ -0,0 +1,10 @@
+using ClimateMonitor;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
