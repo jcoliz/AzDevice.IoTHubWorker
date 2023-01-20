6e8dcab202acfd4348b53addb6507575649273e2
James Coliz <jcoliz@outlook.com>
2023-01-19 21:22:04 -0800

    build(devops): Try to fix ci build id using ps

--- examples/TemperatureController/scripts/Update-Local-Version.ps1
+++ examples/TemperatureController/scripts/Update-Local-Version.ps1
@@ -1,2 +1,12 @@
-$Version = git describe --always
-Write-Output "{ ""Version"": ""local-$Version"" }"
+if (Test-Path env:BUILD_BUILDID) 
+{
+    $Version = "ci-$env:BUILD_BUILDID"
+
+}
+else 
+{    
+    $Commit = git describe --always
+    $Version = "local-$Commit"
+}
+
+Write-Output "{ ""Version"": ""$Version"" }"
