ce63dfd5ce295c7f68ff971166b31cb4cb5df770
James Coliz <jcoliz@outlook.com>
2023-01-25 14:30:31 -0800

    build(ado): First attempt at DEB publishing

--- examples/TemperatureController/deb/dpkg-deb.sh
+++ examples/TemperatureController/deb/dpkg-deb.sh
@@ -4,7 +4,7 @@ cp deb/tempcontroller.service publish_output/etc/systemd/system
 mkdir -p publish_output/DEBIAN
 chmod 0775 publish_output/DEBIAN
 cp deb/control publish_output/DEBIAN
-echo "Package: $1" >> publish_output/DEBIAN/control
-echo "Version: $2" >> publish_output/DEBIAN/control
-echo "Architecture: $3" >> publish_output/DEBIAN/control
-dpkg-deb --build publish_output/ $1-$2-$3.deb
+echo "Package: $2" >> publish_output/DEBIAN/control
+echo "Version: $3" >> publish_output/DEBIAN/control
+echo "Architecture: $4" >> publish_output/DEBIAN/control
+dpkg-deb --build publish_output/ $1/$2_$3_$4.deb
