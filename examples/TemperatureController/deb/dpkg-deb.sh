fa4513cc4278f033891b30cc42fd9869323fdd23
James Coliz <jcoliz@outlook.com>
2023-01-25 19:10:12 -0800

    build(ado): Fix error with latest deb build
    dpkg-deb: error: maintainer script 'postinst' has bad permissions 644 (must be >=0555 and <=0775)

--- examples/TemperatureController/deb/dpkg-deb.sh
+++ examples/TemperatureController/deb/dpkg-deb.sh
@@ -2,10 +2,10 @@
 mkdir -p publish_output/etc/systemd/system
 cp deb/tempcontroller.service publish_output/etc/systemd/system
 mkdir -p publish_output/DEBIAN
-chmod 0775 publish_output/DEBIAN
 cp deb/control publish_output/DEBIAN
 cp deb/postinst publish_output/DEBIAN
 cp deb/prerm publish_output/DEBIAN
+chmod -R 0775 publish_output/DEBIAN
 echo "Package: $2" >> publish_output/DEBIAN/control
 echo "Version: $3" >> publish_output/DEBIAN/control
 echo "Architecture: $4" >> publish_output/DEBIAN/control
