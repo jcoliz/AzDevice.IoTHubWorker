50c2fd6e5689a5248300a87071eafcdb79dc9214
James Coliz <jcoliz@outlook.com>
2023-01-25 19:18:47 -0800

    build(ado): Fix deb uninstall by switching to postrm script
    (Not prerm)

--- examples/TemperatureController/deb/dpkg-deb.sh
+++ examples/TemperatureController/deb/dpkg-deb.sh
@@ -4,7 +4,7 @@ cp deb/tempcontroller.service publish_output/etc/systemd/system
 mkdir -p publish_output/DEBIAN
 cp deb/control publish_output/DEBIAN
 cp deb/postinst publish_output/DEBIAN
-cp deb/prerm publish_output/DEBIAN
+cp deb/postrm publish_output/DEBIAN
 chmod -R 0775 publish_output/DEBIAN
 echo "Package: $2" >> publish_output/DEBIAN/control
 echo "Version: $3" >> publish_output/DEBIAN/control
