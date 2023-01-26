06baf86ff7e5d7994802b1d9855dd3d6d9390310
James Coliz <jcoliz@outlook.com>
2023-01-25 19:39:48 -0800

    build(ado): Refactor dpkg-deb to copy entire DEBIAN directory at once

--- examples/TemperatureController/deb/dpkg-deb.sh
+++ examples/TemperatureController/deb/dpkg-deb.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 mkdir -p publish_output/etc/systemd/system
 cp deb/tempcontroller.service publish_output/etc/systemd/system
-mkdir -p publish_output/DEBIAN
-cp deb/control publish_output/DEBIAN
-cp deb/postinst publish_output/DEBIAN
-cp deb/postrm publish_output/DEBIAN
-cp deb/prerm publish_output/DEBIAN
+cp -R deb/DEBIAN publish_output/
 chmod -R 0775 publish_output/DEBIAN
 echo "Package: $2" >> publish_output/DEBIAN/control
 echo "Version: $3" >> publish_output/DEBIAN/control
