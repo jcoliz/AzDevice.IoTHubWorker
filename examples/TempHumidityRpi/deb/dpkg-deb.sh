03d2600b21165ab2b30f673dff8ac4e1b048cb17
James Coliz <jcoliz@outlook.com>
2023-02-10 16:59:27 -0800

    build(deb): Specify xz compression for deb's headed to RPi

--- examples/TempHumidityRpi/deb/dpkg-deb.sh
+++ examples/TempHumidityRpi/deb/dpkg-deb.sh
@@ -6,4 +6,4 @@ chmod -R 0775 publish_output/DEBIAN
 echo "Package: $2" >> publish_output/DEBIAN/control
 echo "Version: $3" >> publish_output/DEBIAN/control
 echo "Architecture: $4" >> publish_output/DEBIAN/control
-dpkg-deb --build publish_output/ $1/$2_$3_$4.deb
+dpkg-deb -Zxz --build publish_output/ $1/$2_$3_$4.deb
