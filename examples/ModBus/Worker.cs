81777bc6e4da3da25b175f4bc02d406e5c47d492
James Coliz <jcoliz@outlook.com>
2023-02-13 11:16:13 -0800

    build(examples): First working connection to ModBus
    Also add build/debug tasks

--- examples/ModBus/Worker.cs
+++ examples/ModBus/Worker.cs
@@ -1,19 +1,43 @@
+using System.IO.Ports;
+using FluentModbus;
+
 namespace ModBus;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly ModbusRtuClient _client;
 
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
+
+        string[] ports = SerialPort.GetPortNames();
+        var portsall = string.Join(',', ports);
+        _logger.LogInformation("Available ports: {ports}", portsall);
+
+        _client = new ModbusRtuClient()
+        {
+            BaudRate = 9600,
+            Parity = Parity.None,
+            StopBits = StopBits.One
+        };
+
+        _client.Connect("COM3",ModbusEndianness.BigEndian);
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            var inputs = _client.ReadInputRegisters<Int16>(1,1,2).ToArray();
+
+            _logger.LogInformation("Temp: {temp}, Humidity: {rh}", inputs[0], inputs[1]);
+
+            var holdings = _client.ReadHoldingRegisters<Int16>(1,0x101,4).ToArray();
+
+            _logger.LogInformation("Address: {addr}, Baud: {baud}, Temp Cor {tc}, Humidity Cor {hc}", holdings[0], holdings[1], holdings[2], holdings[3]);
+
             await Task.Delay(1000, stoppingToken);
         }
     }
