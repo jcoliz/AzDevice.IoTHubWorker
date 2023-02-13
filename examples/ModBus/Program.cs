486158ead7ced83949973856d3cc6363a500cec6
James Coliz <jcoliz@outlook.com>
2023-02-13 10:38:06 -0800

    build: dotnet new worker -o ModBus

---
+++ examples/ModBus/Program.cs
@@ -0,0 +1,10 @@
+using ModBus;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
