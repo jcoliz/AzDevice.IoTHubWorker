470849d918ecd506bd7e7e30eae792a85ba42cbd
James Coliz <jcoliz@outlook.com>
2023-02-15 20:42:39 -0800

    feat(examples): Implement writing registers to SM7820

--- examples/ModBusSensor/Models/SonbestSm7820Model.cs
+++ examples/ModBusSensor/Models/SonbestSm7820Model.cs
@@ -2,9 +2,20 @@
 
 using AzDevice.Models;
 using FluentModbus;
+using System.Collections.Concurrent;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
+/// <summary>
+/// IoT Plug and Play implementation for SonBest SM7820B RS485 Temp/Humidity sensor
+/// </summary>
+/// <remarks>
+/// http://www.sonbest.com/english/products/SM7820B.html
+/// 
+/// Note that this sensor seems to be temperamental to multiple read/write operations within 5-10
+/// seconds of each other. This causes quite a bit of implementation complexity which can be
+/// avoided by choosing another sensor.
+/// </remarks>
 public class SonbestSm7820Model :  IComponentModel
 {
     #region Properties
@@ -14,7 +25,23 @@ public class SonbestSm7820Model :  IComponentModel
 
     public int Address { get; private set; }
 
-    public int ModelCode { get; private set; } = -1;
+    public int ModelCode
+    { 
+        get
+        {
+            if (ConfigRegisterCache is not null)
+                return ConfigRegisterCache[ModelCodeRegister - FirstConfigRegister];
+            else
+                return -1;
+        }
+        private set
+        {
+            // The data sheet seems to indicate that we can WRITE a new model code.
+            // It's not clear what is the use backing that, so we are not implementing
+            // this functionality in software.
+            throw new NotImplementedException();
+        }
+    }
 
     public enum BaudRateEnum 
     {
@@ -27,11 +54,56 @@ public class SonbestSm7820Model :  IComponentModel
         Baud115200 = 6
     }
 
-    public int BaudRate { get; private set; } = -1;
+    public int BaudRate 
+    { 
+        get
+        {
+            if (ConfigRegisterCache is not null)
+                return ConfigRegisterCache[BaudRateRegister - FirstConfigRegister];
+            else
+                return -1;
+        }
+        private set
+        {
+            // Not implemented, ignored right now
+            // Changing the baud rate is a risky operation.
+            // This sensor is so temperamental as it is, I really don't want to risk this.            
+        }
+    }
 
-    public double TemperatureCorrection { get; private set; } = 0.0;
+    public double TemperatureCorrection
+    { 
+        get
+        {
+            if (ConfigRegisterCache is not null)
+                return (double)ConfigRegisterCache[TemperatureCorrectionRegister - FirstConfigRegister] / 100.0;
+            else
+                return 0;
+        }
+        private set
+        {
+            // Queue up the register change for later when it's a good time
+            short newval = (short)(value * 100.0);
+            RegisterWriteQueue.Enqueue((TemperatureCorrectionRegister, newval));
+        }
+    }
 
-    public double HumidityCorrection { get; private set; } = 0.0;
+    public double HumidityCorrection 
+    { 
+        get
+        {
+            if (ConfigRegisterCache is not null)
+                return (double)ConfigRegisterCache[HumidityCorrectionRegister - FirstConfigRegister] / 100.0;
+            else
+                return 0;
+        }
+        private set
+        {
+            // Queue up the register change for later when it's a good time
+            short newval = (short)(value * 100.0);
+            RegisterWriteQueue.Enqueue((HumidityCorrectionRegister, newval));
+        }
+    }
 
     public double CurrentTemperature { get; private set; }
 
@@ -43,6 +115,8 @@ public class SonbestSm7820Model :  IComponentModel
     const int FirstDataRegister = 0;
     const int TemperatureRegister = 0;
     const int HumidityRegister = 1;
+    const int AfterLastDataRegister = 2;
+    
     const int FirstConfigRegister = 0x64;
     const int ModelCodeRegister = 0x64;
     const int AddressRegister = 0x66;
@@ -82,8 +156,31 @@ public class SonbestSm7820Model :  IComponentModel
     #endregion
 
     #region Internals
+
+    /// <summary>
+    /// Which modbus client to use for communication
+    /// </summary>
     [JsonIgnore]
     public ModbusRtuClient? ModBusClient { get; set; }
+
+    /// <summary>
+    /// Cache of registers from the sensor
+    /// </summary>
+    /// <remarks>
+    /// Allows us to read them all in one operation, and provide them to callers
+    /// as-needed
+    /// </remarks>
+    private short[]? ConfigRegisterCache;
+
+    /// <summary>
+    /// Queue of write operations
+    /// </summary>
+    /// <remarks>
+    /// Allows us to control the timing of write operations for when we think will
+    /// be most successful, given the temperamental nature of this sensor. Also lets
+    /// us retain the desired state until the write operation IS finally done.
+    /// </remarks>
+    private readonly ConcurrentQueue<(int,short)> RegisterWriteQueue = new ConcurrentQueue<(int,short)>();
 
     private bool UartOK => ModBusClient is not null && Address > 0;
     #endregion
@@ -102,16 +199,21 @@ public class SonbestSm7820Model :  IComponentModel
     /// <returns>All telemetry we wish to send at this time, or null for don't send any</returns>
     object? IComponentModel.GetTelemetry()
     {
+        // NOTE: I really need to refactor all this. Because the sensor is so sensitive to read/write timing,
+        // I should have a single background thread which is independently responsible for ALL interaction
+        // with the sensor. Even telemetry is read in there. Then all the customer-facing use of this class
+        // will always get cached values.
+
         if (!UartOK)
             return null;
 
         // Read input registers
-        var inputs = ModBusClient!.ReadHoldingRegisters<Int16>(Address,TemperatureRegister,2).ToArray();
+        var inputs = ModBusClient!.ReadHoldingRegisters<Int16>(Address,FirstDataRegister,AfterLastDataRegister-FirstDataRegister).ToArray();
 
         // Save those as telemetry
         var reading = new Telemetry();
-        reading.Temperature = (double)inputs[0] / 100.0;
-        reading.Humidity = (double)inputs[1] / 100.0;
+        reading.Temperature = (double)inputs[TemperatureRegister-FirstDataRegister] / 100.0;
+        reading.Humidity = (double)inputs[HumidityRegister-FirstDataRegister] / 100.0;
 
         // Update the properties which track the current values
         CurrentHumidity = reading.Humidity;
@@ -125,18 +227,36 @@ public class SonbestSm7820Model :  IComponentModel
 
             try
             {
-                var configs = ModBusClient!.ReadHoldingRegisters<Int16>(Address, FirstConfigRegister, AfterLastConfigRegister - FirstConfigRegister).ToArray();
+                ConfigRegisterCache = ModBusClient!.ReadHoldingRegisters<Int16>(Address, FirstConfigRegister, AfterLastConfigRegister - FirstConfigRegister).ToArray();
 
-                ModelCode = configs[ModelCodeRegister - FirstConfigRegister];
-                BaudRate = configs[BaudRateRegister - FirstConfigRegister];
-                TemperatureCorrection = configs[TemperatureCorrectionRegister - FirstConfigRegister];
-                HumidityCorrection = configs[HumidityCorrectionRegister - FirstConfigRegister];
+                // Process any pending register writes
+                if (! RegisterWriteQueue.IsEmpty)
+                {
+                    // Peek the top item
+                    if (RegisterWriteQueue.TryPeek(out var top))
+                    {
+                        var (register, value) = top;
 
+                        // Don't write the value through to the sensor IF it already has that value!
+                        if (ConfigRegisterCache[register-FirstConfigRegister] != value) 
+                        {
+                            await Task.Delay(TimeSpan.FromSeconds(5));
+
+                            ModBusClient.WriteSingleRegister(Address, register, value);
+                        }
+
+                        // Note that we don't DEQUEUE it until it's successfully been applied without error
+                        RegisterWriteQueue.TryDequeue(out _);
+                    }
+
+                    // NOTE that we only do ONE change per run through telemetry
+                }
             }
             catch
             {
                 // Experimenting with how to communicate that this failed.
-                ModelCode = -2;
+                if (ConfigRegisterCache is not null)
+                    ConfigRegisterCache[ModelCodeRegister - FirstConfigRegister] = -2;
             }
         });
 
